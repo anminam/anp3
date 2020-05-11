49feff2014076333b40851f27f10733d5cdab366
Anminam <minamhunmin@gmail.com>
2020-05-12 08:14:59 +0900

    sample use html

--- examples/index.js
+++ examples/index.js
@@ -2,10 +2,11 @@ const anp3 = require('../dist/anp3');
 
 const start = async () => {
     try {
-
+        console.log("시작");
+        debugger;
         const a = anp3.start();
         a().then((value) => {
-            value
+            console.log(value);
 
         })
     } catch(e) {
