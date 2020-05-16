19caab13526c3d474d109e93e4a9d97c1c67c8f2
minamAn <minamhunmin@gmail.com>
2020-05-16 09:37:24 +0900

    update findObj

--- utils/index.js
+++ utils/index.js
@@ -1,18 +1,14 @@
 const fs = require('fs');
 const getHtml = require('./getHtml');
+const _ = require('lodash');
 
 const findObj = (list, key, value) => {
-    let obj = null;
-    list.some(item => {
-        if( item[key] == value) {
-            obj = item;
-            return true;
-        }
-
-    });
+    const obj = _.find(list, (o) => (
+        o[key] === value
+    ))
     return obj;
-};
 
+}
 const getShortSong = (name) => {
     const arr = name.split('(');
     return arr[0].trim();
